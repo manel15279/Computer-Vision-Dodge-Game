ceee47d6747894548ec104fcbd3e6f1872a3ebb5
manel15279 <manelouchar@users.noreply.github.com>
2024-02-17 22:32:05 +0100

    cleaning

--- interfaceFinal/game_logic.py
+++ interfaceFinal/game_logic.py
@@ -91,7 +91,7 @@ class Player:
         self.h = 40
         self.x = width // 2
         self.y = height - self.h
-        self.player_character = cv2.imread("chicken.png", cv2.IMREAD_UNCHANGED)  
+        self.player_character = cv2.imread("interfaceFinal\\chicken.png", cv2.IMREAD_UNCHANGED)  
 
 
     def move_left(self):
@@ -117,7 +117,7 @@ class Enemy:
         self.x = random.randint(28, width - 28)
         self.y = 0 - self.h
         self.speed = speed
-        self.enemy_character = cv2.imread("fox.png", cv2.IMREAD_UNCHANGED)
+        self.enemy_character = cv2.imread("interfaceFinal\\fox.png", cv2.IMREAD_UNCHANGED)
 
     def collision(self, obj):
         x_overlap = max(0, min(obj.x + obj.w, self.x + self.w) - max(obj.x, self.x))
@@ -150,7 +150,7 @@ class BorderEnemy:
         self.x = x
         self.y = random.randint(-height, 0)
         self.speed = speed
-        self.border_enemy_character = cv2.imread("tree.png", cv2.IMREAD_UNCHANGED)
+        self.border_enemy_character = cv2.imread("interfaceFinal\\tree.png", cv2.IMREAD_UNCHANGED)
 
     def collision(self, obj):
         x_overlap = max(0, min(obj.x + obj.w, self.x + self.w) - max(obj.x, self.x))
@@ -246,7 +246,7 @@ while True:
     frame=resize(frame)
     cv2.flip(frame,1, frame)
 
-    img = cv2.imread("bg.png") 
+    img = cv2.imread("interfaceFinal\\bg.png") 
 
     # Si le jeu est activé
     if game_mode:
@@ -351,6 +351,5 @@ while True:
             player.move_left()
         elif key == ord('d'):#deplacer droite
             player.move_right(width)
-
 
 cv2.destroyAllWindows()
